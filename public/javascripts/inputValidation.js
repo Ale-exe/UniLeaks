79df73d98d5f2243174461818815e46799910ccb
jamesatkinson2002 <154442374+jamesatkinson2002@users.noreply.github.com>
2024-03-20 22:21:20 +0000

    Added validateRegisterForm() to inputValidation.js

--- public/javascripts/inputValidation.js
+++ public/javascripts/inputValidation.js
@@ -10,3 +10,43 @@ function validateWordcount(postTitle, postBody){
         return true;
     }
 }
+//Input validation for the sign up form
+function validateRegisterForm(){
+    const username = document.getElementById('inputUsername').value.trim();
+    const password = document.getElementById('inputPassword').value.trim();
+    const email = document.getElementById('inputEmail').value.trim();
+
+    if(username.length <= 0){
+        document.getElementById("usernameMsg").innerHTML = "Please enter a username";
+        return false;
+    }    
+    if(password.length <= 0) {
+        document.getElementById("passwordMsg").innerHTML = "Please enter a password"
+        return false;
+    }
+    if(password.length < 12) {
+        document.getElementById("passwordMsg").innerHTML = "Password length must be at least 12 characters long";
+        return false; 
+    }
+    if(!(/[a-z]/.test(password))){
+        document.getElementById("passwordMsg").innerHTML = "Password must contain a lower case character";
+        return false;
+    }
+    if(!(/[A-Z]/.test(password))){
+        document.getElementById("passwordMsg").innerHTML = "Password must contain an upper case character";
+        return false;
+    }
+    if(!(/\d/.test(password))){
+        document.getElementById("passwordMsg").innerHTML = "Password must contain a number";
+        return false;
+    }
+    if(!(/[#.?!@$%^&*-]/.test(password))){
+        document.getElementById("passwordMsg").innerHTML = "Password must contain a special character: #.?!@$%^&*-";
+        return false;
+    }  
+    if(!(/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/.test(email))){
+        document.getElementById("emailMsg").innerHTML = "Please enter a valid email address";
+        return false;
+    }
+    return true;
+}
