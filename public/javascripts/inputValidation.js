9135f52861ac6d39521684489657ec846a1e9614
Alex <alexlgwilliam@gmail.com>
2024-03-20 22:04:13 +0000

    code comment

--- public/javascripts/inputValidation.js
+++ public/javascripts/inputValidation.js
@@ -1,6 +1,6 @@
+// Validates create post form to ensure it doesn't go over or under character limit
 function validateWordcount(postTitle, postBody){
     if (postTitle.trim().length < 1 || postBody.trim().length < 1){
-        // console.log("false")
         return false;
     } else if (postTitle.trim().length > 50){
         return false;
